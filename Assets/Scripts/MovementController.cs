1c8399ffe754f54f6990d0312c4e6dc8913ae956
Duy Thai <duy.thai@altsrc.net>
2019-12-05 17:44:11 +0700

    scripts

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+public class MovementController : MonoBehaviour
+{
+	public float movementSpeed = 3.0f;
+	Vector2 movement = new Vector2();
+	Rigidbody2D rb2D;
+
+	private void Start()
+	{
+		rb2D = GetComponent<Rigidbody2D>();
+	}
+	private void Update()
+	{
+	}
+
+	void FixedUpdate()
+	{
+		movement.x = Input.GetAxisRaw("Horizontal");
+		movement.y = Input.GetAxisRaw("Vertical");
+
+		movement.Normalize();
+
+		rb2D.velocity = movement * movementSpeed;
+	}
+}
